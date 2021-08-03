0baf0e344fc4361052acecc5f7a998c484a9e031
Lacho123 <44599728+Lacho123@users.noreply.github.com>
2021-08-03 16:30:15 +0300

    Add files via upload

---
+++ Books'/Books'/BookComparator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class BookComparator
+    {
+        public int Compare(Book first, Book second)
+        {
+            int result = first.Title.CompareTo(second.Title);
+            if (result == 0)
+            {
+                return first.Year.CompareTo(second.Year);
+            }
+            return result;
+        }
+    }
+}
---
+++ Books'/Books'/Library.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books { get; }
+        public IEnumerator<Book> GetEnumerator()
+        {
+            LibraryIterator IBookIterator = new LibraryIterator(this.books);
+            return IBookIterator;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private readonly List<Book> books;
+            private int index;
+
+            public Book Current
+            {
+                get
+                {
+                    return this.books[this.index];
+                }
+            }
+
+            object IEnumerator.Current
+            {
+                get
+                {
+                    return this.Current;
+                }
+            }
+
+            public LibraryIterator(List<Book> books)
+            {
+                this.Reset();
+                this.books = books;
+            }
+
+            public void Dispose()
+            {
+                GC.SuppressFinalize(this);
+            }
+
+            public bool MoveNext()
+            {
+                this.index++;
+                return this.index < this.books.Count;
+            }
+
+            public void Reset()
+            {
+                this.index = -1;
+            }
+        }
+
+    }
+}
