d0d1df1e0e9233c51a346e6638266972a9a6c126
Lacho123 <44599728+Lacho123@users.noreply.github.com>
2021-08-03 16:25:42 +0300

    Add files via upload

---
+++ CarManufacturer/CarManufacturer/Car.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+        private Engine engine;
+        private Tire[] tires;
+
+        public string Make
+        {
+            get { return this.make; }
+            set { this.make = value; }
+        }
+        public string Model
+        {
+            get { return this.model; }
+            set { this.model = value; }
+        }
+        public int Year
+        {
+            get { return this.year; }
+            set { this.year = value; }
+        }
+        public double FuelQuantity
+        {
+            get { return this.fuelQuantity; }
+            set { this.fuelQuantity = value; }
+        }
+        public double FuelConsumption
+        {
+            get { return this.fuelConsumption; }
+            set { this.fuelConsumption = value; }
+        }
+
+        public Engine Engine
+        {
+            get { return this.engine; }
+            set { this.engine = value; }
+        }
+
+        public Tire[] Tires
+        {
+            get { return this.tires; }
+            set { this.tires = value; }
+        }
+
+        public Car()
+        {
+            this.Make = "VW";
+            this.Model = "Golf";
+            this.Year = 2025;
+            this.FuelQuantity = 200;
+            this.FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year)
+            : this()
+        {
+            
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+            : this(make, model, year)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires)
+            : this(make, model, year, fuelQuantity, fuelConsumption)
+        {
+            this.Engine = engine;
+            this.Tires = tires;
+        }
+
+        public void Drive(double distance)
+        {
+            if(this.FuelQuantity - (distance * this.FuelConsumption) > 0)
+            {
+                this.FuelQuantity -= (distance * this.FuelConsumption);
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}L" ;
+        }             
+    }
+
+    public class Engine
+    {
+        private int horsePower;
+        private double cubicCapacity;
+
+        public int HorsePower
+        {
+            get { return horsePower; }
+            set { horsePower = value; }
+        }
+
+        public double CubicCapacity
+        {
+            get { return cubicCapacity; }
+            set { cubicCapacity = value; }
+        }
+
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            this.HorsePower = horsePower;
+            this.CubicCapacity = cubicCapacity;
+        }
+
+    }
+
+    public class Tire
+    {
+        private int year;
+        private double pressure;
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public double Pressure
+        {
+            get { return pressure; }
+            set { pressure = value; }
+        }
+
+        public Tire(int year, double pressure)
+        {
+            this.Year = year;
+            this.Pressure = pressure;
+        }
+
+    }
+}
